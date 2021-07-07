2f40b7d8a6f5f1850f9e16101e8c284f6785298c
iknoom <52124204+iknoom@users.noreply.github.com>
2021-07-08 03:51:49 +0900

    Initialize spring boot

---
+++ null-backend/src/main/java/swmaestronull/nullbackend/NullBackendApplication.java
@@ -0,0 +1,13 @@
+package swmaestronull.nullbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullBackendApplication.class, args);
+	}
+
+}
