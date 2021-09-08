ddfe1faaaeb101570ddaf3d2e4b7f3ae1c265e0c
iknoom <52124204+iknoom@users.noreply.github.com>
2021-09-09 03:05:03 +0900

    feat: implement userDetail

--- src/main/java/swmaestronull/nullbackend/domain/user/User.java
+++ src/main/java/swmaestronull/nullbackend/domain/user/User.java
@@ -1,15 +1,23 @@
 package swmaestronull.nullbackend.domain.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @JsonIgnore
     @Id
@@ -29,4 +37,48 @@ public class User {
     @JsonIgnore
     @Column
     private boolean activated;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    @Builder
+    public User(String email, String password, String nickname, boolean activated, List<String> roles) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.activated = activated;
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
--- src/main/java/swmaestronull/nullbackend/domain/user/UserReposiroty.java
+++ src/main/java/swmaestronull/nullbackend/domain/user/UserReposiroty.java
@@ -2,5 +2,9 @@ package swmaestronull.nullbackend.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserReposiroty extends JpaRepository<User, Long> {
+
+    Optional<User> findOneWithAuthoritiesByEmail(String email);
 }
