6390572c4e326e37b2fbad6b77c002748c6654e8
iknoom <52124204+iknoom@users.noreply.github.com>
2021-10-13 22:27:43 +0900

    fix: solve CORS error

--- src/main/java/swmaestronull/nullbackend/web/UserController.java
+++ src/main/java/swmaestronull/nullbackend/web/UserController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@CrossOrigin(origins = "*")
 public class UserController {
     private final UserService userService;
 
